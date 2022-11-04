ebba99e3787cd3513c3f16b7df5bca04d78c7ec0
MikeTeddyOmondi <mike_omondi@outlook.com>
2022-11-04 17:24:37 +0300

    Updated entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#python manage.py migrate --noinput
+python manage.py makeigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn todo_django_api.wsgi:application --bind 0.0.0.0:8000
+gunicorn mpesa_api.wsgi:application --bind 0.0.0.0:8000
